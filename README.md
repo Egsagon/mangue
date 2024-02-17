56b150ae284064983706cf12fc6a7e04b12d84e1
EGsagon <egsagon12@gmail.com>
2024-02-17 12:55:46 +0100

    Fix 403

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 
 Mangas.io downloader
 
-> !IMPORTANT
-> Broken as of now, don't use
+> [!WARNING]
+> This is probably against mangas.io terms or service.
+
+> [!CAUTION]
+> I don't know if they will ban accounts for this, so avoid mass downloading. You may want to fine-tune the delays in the configuration according to your download needs.
 
 ## Installation
 ```sh
@@ -13,6 +16,33 @@ pip install -r requirements.txt
 py main.py
 ```
 
+## Usage
+
+1. Get a manga slug (found in mangas URL)
+    - For exemple: https://www.mangas.io/lire/undead-unluck
+    - The manga slug will be undead-unluck
+
+2. Start script
+    - `py main.py`
+
+```
+[ + ] Email > my@mail.com
+[ + ] Password > *************
+[ > ] Sucess! Logged in as `my@mail.com`
+[ + ] Manga slug > undead-unluck
+[ > ] Manga found! Has 134 chapters available.
+[ + ] Select chapter start (default=1) >
+[ + ] Select chapter end  (default=-1) > 2
+[ > ] Downloading 1 chapters. Press enter to confirm >
+...
+[ > ] Success! CBZ archive has been saved as `undead-unluck-ch1.cbz`.
+```
+
+> [!NOTE]
+> You can create a `creds.json` file in cwd to automatically login. Set `email` and `password` keys.
+
 # License
 
 Licensed under GPLv3. See the `LICENSE` file.
+
+The Software is provided “as is”, without warranty of any kind, express or implied, In no event shall the authors or copyright holders be liable for any claim, damages or other liability, whether in an action of contract, tort or otherwise, arising from, out of or in connection with the software or the use or other dealings in the Software.
--- main.py
+++ main.py
@@ -1,39 +1,53 @@
 '''
     Mangas.io downloader
-    @Egsagon
+    github.com/Egsagon/mangue
+    License: GPLv3
 '''
 
 import os
 import time
 import tqdm
 import json
+
 import pwinput
 import zipfile
 import requests
 
-if os.path.exists('./creds.json'):
-    print('[*] Using creds file')
-    with open('./creds.json') as file:
-        creds = json.load(file)
+# ============== Configuration ============== #
 
-else:
-    creds = {
-        'email': input('[*] Email > '),
-        'password': pwinput.pwinput('[*] Password > ')
-    }
+MAX_ATTEMPTS = 3
+ATTEMPT_DELAY = 3
+CHAPTER_INTERVAL_DELAY = 2
+PROXIES: dict = None
 
-with open('ch.graphql') as file:
-    ch_query = file.read()
+# =========================================== #
 
-with open('vol.graphql') as file:
-    vol_query = file.read()
-
-session = requests.Session()
+BAR_FORMAT = '{desc} {bar} {percentage:.1f}% [{elapsed}<{remaining}]'
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0',
     'Content-Type': 'application/json; charset=utf-8'
 }
+
+with open('query.graphql') as file:
+    graphql = file.read()
+
+session = requests.Session()
+
+# ============== Get credentials ============== #
+
+if os.path.exists('./creds.json'):
+    print('[ > ] Using creds file')
+    with open('./creds.json') as file:
+        creds = json.load(file)
+
+else:
+    creds = {
+        'email': input('[ + ] Email > '),
+        'password': pwinput.pwinput('[ + ] Password > ')
+    }
+
+# ============== Authentification ============== #
 
 auth = session.post(
     url = 'https://api.mangas.io/auth/login',
@@ -42,110 +56,119 @@ auth = session.post(
 )
 
 if not auth.ok:
-    print(f'[-] \033[91mError {auth.status_code}: {auth.reason}\033[0m')
+    print(f'[ - ] \033[91mError {auth.status_code}: {auth.reason}\033[0m')
     exit()
 
 auth_data = auth.json()
 user_data = auth_data.get('user')
 user_token = auth_data.get('token')
 
-print(f'[>] \033[92mSuccess! Logged in as `{user_data["email"]}`\033[0m')
+print(f'[ > ] \033[92mSuccess! Logged in as `{user_data["email"]}`\033[0m')
 
 headers['authorization'] = 'Bearer ' + user_token
 
-manga_slug = input('[*] Manga slug > ')
+# ============== Check if manga exists ============== #
 
-payload = {
-    'operationName': 'getVolumes',
-    'variables': {'slug': manga_slug},
-    'query': vol_query
-}
+def query(_operation: str, _variables: dict[str]) -> dict | list:
+    '''
+    Perform a GraphQL query.
+    '''
+    
+    payload = {
+        'operationName': _operation,
+        'variables': _variables,
+        'query': graphql
+    }
 
-res = session.post(
-    'https://api.mangas.io/api',
-    data = json.dumps(payload, separators = (',', ':')),
-    headers = headers | {'Content-Length': str(len(creds))}
-)
+    res = session.post(
+        'https://api.mangas.io/api',
+        data = json.dumps(payload, separators = (',', ':')),
+        headers = headers
+    )
+    
+    if not res.ok:
+        print(f'[-] \033[91mError {res.status_code}: {res.reason}\033[0m')
+        exit()
+    
+    return res.json()['data']
 
-if not res.ok:
-    print(f'[-] \033[91mError {res.status_code}: {res.reason}\033[0m')
+manga_slug = input('[ + ] Manga slug > ')
+
+res = query('exists', dict(slug = manga_slug))['manga']
+
+if not res:
+    print('[ - ] \033[91mError: Manga not found.\033[0m')
     exit()
+    
+chapters_count = res['chapterCount']
 
-manga = res.json()['data']['manga']
+print(f'[ > ] \033[92mManga found! Has {chapters_count} chapters available.\033[0m')
 
-if not manga:
-    print('[-] \033[91mError: Manga not found.\033[0m')
-    exit()
+# ============== Select download range ============== #
 
-volumes = manga['volumes']
-
-chapters = []
-for volume in volumes:
-    chapters += volume['chapters']
-
-chapters_count = len(chapters)
-
-print(f'[>] \033[92mSucess! This manga has {len(volumes)} volumes and {chapters_count} chapters\033[0m')
-
-start = int(input('[*] Select chapter start (default=0) > ') or 0 )
-end =   int(input('[*] Select chapter end  (default=-1) > ') or -1)
+start = int(input('[ + ] Select chapter start (default=1) > ') or +1)
+end   = int(input('[ + ] Select chapter end  (default=-1) > ') or -1)
 
 if start < 0: start = chapters_count - start
 if end   < 0: end   = chapters_count - end
 
-print(f'[>] Downloading {end - start} chapters.')
-input('[*] Press enter to confirm > ')
+print(f'[ > ] Downloading {end - start} chapters.')
+input('[ + ] Press enter to confirm > ')
 
-if start == 0 and end == chapters_count - 1:
+if start == 1 and end == chapters_count - 1:
     archive_name = f'{manga_slug}.cbz'
 
 else:
     archive_name = f'{manga_slug}-ch{start}-ch{end}.cbz'
 
 if os.path.exists(archive_name):
-    print('[-] \033[91mError: CBZ archive already exists in cwd.')
+    print('[ - ] \033[91mError: CBZ archive already exists in cwd.')
     exit()
+
+# ============== Start download ============== #
 
 with zipfile.ZipFile(archive_name, 'w') as archive:
 
-    '''
-    for i in tqdm.tqdm(range(start, end), desc = 'Total'):
-        for page in tqdm.tqdm(chapters[i]['pages'], desc = f'Chapter {i}', leave = False):
+    for chapter in tqdm.tqdm(range(start, end),
+                             desc = 'Progress',
+                             colour = 'magenta',
+                             bar_format = BAR_FORMAT,
+                             ascii = ' ─'):
+        
+        res = query('getChapter', dict(slug = manga_slug, chapterNb = chapter))
+        chapter_data = res['manga']['chapter']
+    
+        for page in tqdm.tqdm(chapter_data['pages'],
+                              desc = f'CH-{str(chapter).zfill(5)}',
+                              leave = False,
+                              bar_format = BAR_FORMAT,
+                              ascii = ' ─'):
+    
+            pid = page['_id']
             
-            url = page['image']['url']
-            page_name = f'ch{i}-p{page["number"]}'
+            page_name = f'ch{chapter}-pg{page["number"]}'
             
-            if page.get('isDoublePage'):
+            if page['isDoublePage']:
                 page_name += '-double'
             
-            for i in range(3):
-                res = session.get(url, headers = headers)
+            for i in range(MAX_ATTEMPTS):
+            
+                res = query('getImage', dict(id = pid, quality = 'HD'))
+                image = res['page']['image']
+                res = session.get(image['url'], headers = headers)
                 
-                if res.ok: break
+                if res.ok:
+                    archive.writestr(page_name + '.webp', res.content)
+                    break
+                
+                print(f'\n[ - ] \033[93mError {res.status_code}: {res.reason} | {res.content}\033[0m')
+                time.sleep(ATTEMPT_DELAY)
             
-                print(f'\n033[93mError {res.status_code}: {res.reason} | {res.content}')
-                time.sleep(10)
-            
-            archive.writestr(page_name + '.webp', res.content)
-    '''
-    
-    for chapter in tqdm.tqdm(range(start, end), desc = 'Total'):
+            else:
+                print(f'\n[ - ] \033[91mError: Failed to fetch image after {MAX_ATTEMPTS} attempts.\033[0m')
         
-        payload = {
-            'operationName': 'getChapter',
-            'variables': {'slug': manga_slug, 'chapterNb': chapter},
-            'query': ch_query
-        }
+        time.sleep(CHAPTER_INTERVAL_DELAY)
 
-        res = session.post(
-            'https://api.mangas.io/api',
-            data = json.dumps(payload, separators = (',', ':')),
-            headers = headers | {'Content-Length': str(len(creds))}
-        )
-        
-        print(res.content)
-        break
-
-print(f'[>] \033[92mSuccess! CBZ archive has been saved as `{archive_name}`.\033[90m')
+print(f'[ > ] \033[92mSuccess! CBZ archive has been saved as `{archive_name}`.\033[90m')
 
 # EOF
